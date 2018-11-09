d25082a63a0f4234211d58d9138c9561249f1399
Inanc Gumus <m@inanc.io>
2018-11-09 13:35:57 +0300

    refactor: path-searcher to remove superflous code

--- 13-loops/exercises/09-word-finder-exercises/02-path-searcher/solution/main.go
+++ 13-loops/exercises/09-word-finder-exercises/02-path-searcher/solution/main.go
@@ -31,14 +31,8 @@ func main() {
 	query := os.Args[1:]
 
 	for _, q := range query {
-	search:
 		for i, w := range words {
 			q, w = strings.ToLower(q), strings.ToLower(w)
-
-			switch q {
-			case "and", "or", "the":
-				break search
-			}
 
 			if strings.Index(w, q) != notFound {
 				fmt.Printf("#%-2d: %q\n", i+1, w)
