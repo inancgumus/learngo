d9948e47c5ddb1a485f8309766707203d4fe6c89
Paul Waldmann <paul@guardoo.com>
2018-12-21 04:09:51 +0800

    Fix grammar

--- 12-switch/exercises/01-richter-scale/main.go
+++ 12-switch/exercises/01-richter-scale/main.go
@@ -25,13 +25,13 @@ package main
 // MAGNITUDE                    DESCRIPTION
 // ---------------------------------------------------------
 // Less than 2.0                micro
-// 2.0 to less than 3.0         very minor
-// 3.0 to less than 4.0         minor
-// 4.0 to less than 5.0         light
-// 5.0 to less than 6.0         moderate
-// 6.0 to less than 7.0         strong
-// 7.0 to less than 8.0         major
-// 8.0 to less than 10.0        great
+// 2.0 and less than 3.0         very minor
+// 3.0 and less than 4.0         minor
+// 4.0 and less than 5.0         light
+// 5.0 and less than 6.0         moderate
+// 6.0 and less than 7.0         strong
+// 7.0 and less than 8.0         major
+// 8.0 and less than 10.0        great
 // 10.0 or more                 massive
 //
 // EXPECTED OUTPUT
