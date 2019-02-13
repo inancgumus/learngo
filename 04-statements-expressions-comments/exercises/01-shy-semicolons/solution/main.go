dddf96b67ae3fbbac8eb5f998a214cd954968574
Inanc Gumus <m@inanc.io>
2019-02-13 12:06:17 +0300

    update: shy semicolons exercise

--- 04-statements-expressions-comments/exercises/01-shy-semicolons/solution/main.go
+++ 04-statements-expressions-comments/exercises/01-shy-semicolons/solution/main.go
@@ -10,7 +10,8 @@ package main
 func main() {
 	// Uncomment the line of code below; then save the file.
 	//
-	// You will see that Go will fix it automatically.
+	// You will see that Gofmt tool will format your code automatically.
+	// https://golang.org/cmd/gofmt/
 	//
 	// This is because, for Go, it doesn't matter whether
 	// you use semicolons between the statements or not.
