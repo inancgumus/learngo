856c4aa8939436bbe5088cd2daaddc710f7bd009
Inanc Gumus <m@inanc.io>
2019-04-30 15:53:36 +0300

    add: variadic funcs

---
+++ 27-functions-advanced/01-variadic-funcs/log-parser-to-variadic/main.go
@@ -0,0 +1,51 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	p := newParser()
+
+	in := bufio.NewScanner(os.Stdin)
+	for in.Scan() {
+		parsed := parse(p, in.Text())
+		update(p, parsed)
+	}
+
+	summarize(p)
+	dumpErrs(in.Err(), err(p))
+}
+
+// summarize summarizes and prints the parsing result
+func summarize(p *parser) {
+	sort.Strings(p.domains)
+
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
+	fmt.Println(strings.Repeat("-", 45))
+
+	for _, domain := range p.domains {
+		fmt.Printf("%-30s %10d\n", domain, p.sum[domain].visits)
+	}
+	fmt.Printf("\n%-30s %10d\n", "TOTAL", p.total)
+}
+
+// dumpErrs simplifies handling multiple errors
+func dumpErrs(errs ...error) {
+	for _, err := range errs {
+		if err != nil {
+			fmt.Println("> Err:", err)
+		}
+	}
+}
---
+++ 27-functions-advanced/01-variadic-funcs/log-parser-to-variadic/parser.go
@@ -0,0 +1,86 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// result stores the parsed result for a domain
+type result struct {
+	domain string
+	visits int
+	// add more metrics if needed
+}
+
+// parser keep tracks of the parsing
+type parser struct {
+	sum     map[string]result // metrics per domain
+	domains []string          // unique domain names
+	total   int               // total visits for all domains
+	lines   int               // number of parsed lines (for the error messages)
+	lerr    error             // the last error occurred
+}
+
+// newParser constructs, initializes and returns a new parser
+func newParser() *parser {
+	return &parser{sum: make(map[string]result)}
+}
+
+// parse parses a log line and returns the parsed result with an error
+func parse(p *parser, line string) (r result) {
+	if p.lerr != nil {
+		return
+	}
+
+	p.lines++
+
+	fields := strings.Fields(line)
+	if len(fields) != 2 {
+		p.lerr = fmt.Errorf("wrong input: %v (line #%d)", fields, p.lines)
+		return
+	}
+
+	var err error
+
+	r.domain = fields[0]
+	r.visits, err = strconv.Atoi(fields[1])
+
+	if r.visits < 0 || err != nil {
+		p.lerr = fmt.Errorf("wrong input: %q (line #%d)", fields[1], p.lines)
+	}
+	return
+}
+
+// update updates the errors for the given parsing result
+func update(p *parser, r result) {
+	if p.lerr != nil {
+		return
+	}
+
+	// Collect the unique domains
+	if _, ok := p.sum[r.domain]; !ok {
+		p.domains = append(p.domains, r.domain)
+	}
+
+	// Keep track of total and per domain visits
+	p.total += r.visits
+
+	// create and assign a new copy of `visit`
+	p.sum[r.domain] = result{
+		domain: r.domain,
+		visits: r.visits + p.sum[r.domain].visits,
+	}
+}
+
+// err returns the last error encountered
+func err(p *parser) error {
+	return p.lerr
+}
