7569d9d06ffb36a6d1c7f3568dc81acefc252aad
Paul Waldmann <paul@waldmann.io>
2019-11-18 18:26:53 +0800

    fix: minor grammar correction in exercise #24

--- 06-variables/06-project-greeter/exercises/04-greet-more-people/main.go
+++ 06-variables/06-project-greeter/exercises/04-greet-more-people/main.go
@@ -12,9 +12,9 @@ package main
 // EXERCISE: Greet More People
 //
 // RESTRICTIONS
-//  1. Be sure to match to the expected output below
+//  1. Be sure to match the expected output below
 //  2. Store the length of the arguments in a variable
-//  3. Store the all the arguments in variables as well
+//  3. Store all the arguments in variables as well
 //
 // INPUT
 //  bilbo balbo bungo
