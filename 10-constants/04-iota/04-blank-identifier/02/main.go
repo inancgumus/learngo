5d7e245bfe29d57bb9bc280859d3b84b85f4b3f8
anhhh.11 <anhhh.11@gmail.com>
2020-05-25 22:39:09 +0700

    Fix comment

--- 10-constants/04-iota/04-blank-identifier/02/main.go
+++ 10-constants/04-iota/04-blank-identifier/02/main.go
@@ -13,8 +13,8 @@ import "fmt"
 func main() {
 	const (
 		EST = -(5 + iota) // CORRECT: -5
-		MST               // INCORRECT: -6
-		PST               // INCORRECT: -7
+		MST               // CORRECT: -6
+		PST               // CORRECT: -7
 	)
 
 	fmt.Println(EST, MST, PST)
