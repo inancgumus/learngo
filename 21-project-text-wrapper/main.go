a17d4812c482fa6a9b866c3a1f0b9fce203f0135
Mehta Dhruv <dhruvmehta3110@gmail.com>
2021-04-02 21:15:07 +0530

    Update main.go

--- 21-project-text-wrapper/main.go
+++ 21-project-text-wrapper/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	const text = `Galaksinin Batı Sarmal Kolu'nun bir ucunda, haritası bile çıkarılmamış ücra bir köşede, gözlerden uzak, küçük ve sarı bir güneş vardır.
+	const text = `Galaksinin Batı Sarmal Kolu'nun bir ucunda,haritası bile çıkarılmamış ücra bir köşede,gözlerden uzak, küçük ve sarı bir güneş vardır.
 
 Bu güneşin yörüngesinde, kabaca yüz kırksekiz milyon kilometre uzağında, tamamıyla önemsiz ve mavi-yeşil renkli, küçük bir gezegen döner.
 
@@ -28,7 +28,7 @@ Gezegenin maymun soyundan gelen canlıları öyle ilkeldir ki dijital kol saatin
 		fmt.Printf("%c", r)
 
 		switch lw++; {
-		case lw > maxWidth && r != '\n' && unicode.IsSpace(r):
+		case lw > maxWidth && r != '\n' && (unicode.IsSpace(r) || unicode.IsPunct(r)):
 			fmt.Println()
 			fallthrough
 		case r == '\n':
