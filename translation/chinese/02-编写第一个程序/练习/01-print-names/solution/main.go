85980146dc66ed9fc1e578c63547af018850ff46
parkma99 <park-ma@hotmail.com>
2021-12-28 20:30:28 +0800

    write-your-first-program exercises Chinexe version

---
+++ translation/chinese/02-编写第一个程序/练习/01-print-names/solution/main.go
@@ -0,0 +1,21 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// go run main.go
+
+// go build
+// ./solution
+
+func main() {
+	fmt.Println("Nikola")
+	fmt.Println("Thomas")
+}
