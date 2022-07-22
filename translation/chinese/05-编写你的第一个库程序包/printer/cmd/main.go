db8add0b20edd954d4441f1216fdda08e2d109f0
NovaNo1r <mail@novanoir.dev>
2022-07-22 15:59:10 +0800

    Add: 05-write-your-first-library-package Chinese version

---
+++ translation/chinese/05-编写你的第一个库程序包/printer/cmd/main.go
@@ -0,0 +1,16 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// 自动导入！... 好耶！
+import "github.com/inancgumus/learngo/05-write-your-first-library-package/printer"
+
+func main() {
+	printer.Hello()
+}
