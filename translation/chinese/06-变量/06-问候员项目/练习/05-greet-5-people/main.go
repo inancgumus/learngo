9aa46efe83e57b1931b62223a44ca42aeaae8e03
NovaNo1r <mail@novanoir.dev>
2022-10-03 23:24:25 +0800

    Add: 06-variables Chinese version

---
+++ translation/chinese/06-变量/06-问候员项目/练习/05-greet-5-people/main.go
@@ -0,0 +1,36 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// ---------------------------------------------------------
+// 练习: 问候 5 个人
+//
+//  这次问候 5 个人
+//
+//  不要从上一个练习中复制粘贴代码
+//
+// 限制
+//  这次不要用变量
+//
+// 输入
+//  bilbo balbo bungo gandalf legolas
+//
+// 期望输出
+//  There are 5 people!
+//  Hello great bilbo !
+//  Hello great balbo !
+//  Hello great bungo !
+//  Hello great gandalf !
+//  Hello great legolas !
+//  Nice to meet you all.
+// ---------------------------------------------------------
+
+func main() {
+	// 在这写你的代码
+}
