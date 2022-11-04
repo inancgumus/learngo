a08a495b5f99fd79cef5a07779bbb2ea3549925d
Gleb Stepanov <gleb.stepanov@ringcentral.com>
2022-11-03 20:16:04 -0600

    upd

--- 14-arrays/exercises/01-declare-empty/main.go
+++ 14-arrays/exercises/01-declare-empty/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Declare empty arrays
 //
@@ -59,4 +61,36 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	var (
+		names    [3]string
+		distance [5]int
+		data     [5]uint8
+		ratios   [1]float64
+		alives   [4]bool
+		zero     [0]uint8
+	)
+	//%#v	a Go-syntax representation of the value
+	fmt.Printf("names    : %#v\n", names)
+	fmt.Printf("distance    : %#v\n", distance)
+	fmt.Printf("data    : %#v\n", data)
+	fmt.Printf("ratios    : %#v\n", ratios)
+	fmt.Printf("alives    : %#v\n", alives)
+	fmt.Printf("zero    : %#v\n", zero)
+	fmt.Println("\n")
+	//%T	a Go-syntax representation of the type of the value
+	fmt.Printf("names    : %T\n", names)
+	fmt.Printf("distance    : %T\n", distance)
+	fmt.Printf("data    : %T\n", data)
+	fmt.Printf("ratios    : %T\n", ratios)
+	fmt.Printf("alives    : %T\n", alives)
+	fmt.Printf("zero    : %T\n", zero)
+	fmt.Println("\n")
+	//%q	a double-quoted string safely escaped with Go syntax
+	//%f	decimal point but no exponent, e.g. 123.456
+	fmt.Printf("names    : %q\n", names)
+	fmt.Printf("distance    : %d\n", distance)
+	fmt.Printf("data    : %d\n", data)
+	fmt.Printf("ratios    : %.2f\n", ratios)
+	fmt.Printf("alives    : %t\n", alives)
+	fmt.Printf("zero    : %d\n", zero)
 }
