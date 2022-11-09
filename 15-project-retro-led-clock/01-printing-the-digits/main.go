bb340db6b166d89734a736acb0bc56e6ea92499b
Gleb Stepanov <gleb.stepanov@nordigy.ru>
2022-11-08 21:30:08 -0700

    upd

--- 15-project-retro-led-clock/01-printing-the-digits/main.go
+++ 15-project-retro-led-clock/01-printing-the-digits/main.go
@@ -8,5 +8,98 @@
 
 package main
 
+import "fmt"
+
 func main() {
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+	digits := [10]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	for line := range digits[0] {
+		// Print a line for each placeholder in digits
+		for digit := range digits {
+			fmt.Print(digits[digit][line], "  ")
+		}
+		fmt.Println()
+	}
 }
--- 15-project-retro-led-clock/02-printing-the-clock/main.go
+++ 15-project-retro-led-clock/02-printing-the-clock/main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -98,6 +99,13 @@ func main() {
 	digits := [...]placeholder{
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
 
 	for line := range digits[0] {
 		for digit := range digits {
@@ -105,4 +113,28 @@ func main() {
 		}
 		fmt.Println()
 	}
+	now := time.Now()
+	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+	clock := [...]placeholder{
+		digits[hour/10], digits[hour%10],
+		colon,
+		digits[min/10], digits[min%10],
+		colon,
+		digits[sec/10], digits[sec%10],
+	}
+
+	for line := range clock[0] {
+		for digit := range clock {
+			fmt.Print(clock[digit][line], "  ")
+		}
+		fmt.Println()
+	}
+
+	//Create the clock array by getting the digits from the digits array
+	//
+	//Print the clock by using the clock array
+	//
+	//Create a separator array (it's also a placeholder type)
+	//
+	//Add the separators into the correct positions of the clock array
 }
