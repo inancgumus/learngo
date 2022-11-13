1aaa712a4067de47c2f03e5b22ce6c94f4510438
Gleb Stepanov <gleb.stepanov@ringcentral.com>
2022-11-13 15:37:47 -0700

    Prime clock done

---
+++ 15-project-retro-led-clock/exercises/01-refactor/placeholder.go
@@ -0,0 +1,95 @@
+package main
+
+type placeholder [5]string
+
+var zero = placeholder{
+	"███",
+	"█ █",
+	"█ █",
+	"█ █",
+	"███",
+}
+
+var one = placeholder{
+	"██ ",
+	" █ ",
+	" █ ",
+	" █ ",
+	"███",
+}
+
+var two = placeholder{
+	"███",
+	"  █",
+	"███",
+	"█  ",
+	"███",
+}
+
+var three = placeholder{
+	"███",
+	"  █",
+	"███",
+	"  █",
+	"███",
+}
+
+var four = placeholder{
+	"█ █",
+	"█ █",
+	"███",
+	"  █",
+	"  █",
+}
+
+var five = placeholder{
+	"███",
+	"█  ",
+	"███",
+	"  █",
+	"███",
+}
+
+var six = placeholder{
+	"███",
+	"█  ",
+	"███",
+	"█ █",
+	"███",
+}
+
+var seven = placeholder{
+	"███",
+	"  █",
+	"  █",
+	"  █",
+	"  █",
+}
+
+var eight = placeholder{
+	"███",
+	"█ █",
+	"███",
+	"█ █",
+	"███",
+}
+
+var nine = placeholder{
+	"███",
+	"█ █",
+	"███",
+	"  █",
+	"███",
+}
+
+var colon = placeholder{
+	"   ",
+	" ░ ",
+	"   ",
+	" ░ ",
+	"   ",
+}
+
+var digits = [...]placeholder{
+	zero, one, two, three, four, five, six, seven, eight, nine,
+}
---
+++ 15-project-retro-led-clock/exercises/01-refactor/test_import/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("1st file")
+	imported()
+}
