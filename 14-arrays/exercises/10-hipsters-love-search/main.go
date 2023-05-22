58b0012daac0c15255ea2162a4da2b6a4fe3e596
mindy <minzhou16@gmail.com>
2023-05-21 21:24:27 -0400

    completed all exercises for arrays.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- 14-arrays/exercises/10-hipsters-love-search/main.go
+++ 14-arrays/exercises/10-hipsters-love-search/main.go
@@ -8,55 +8,85 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Hipster's Love Bookstore Search Engine
 //
-//  Your goal is to allow people to search for books.
+//	Your goal is to allow people to search for books.
 //
-//  1. Create an array with the following book titles:
-//      Kafka's Revenge
-//      Stay Golden
-//      Everythingship
-//      Kafka's Revenge 2nd Edition
+//	1. Create an array with the following book titles:
+//	    Kafka's Revenge
+//	    Stay Golden
+//	    Everythingship
+//	    Kafka's Revenge 2nd Edition
 //
-//  2. Get the search query from the command-line argument
+//	2. Get the search query from the command-line argument
 //
-//  3. Search for the books in the books array
+//	3. Search for the books in the books array
 //
-//  4. When the program finds the book, print it.
-//  5. Otherwise, print that the book doesn't exist.
+//	4. When the program finds the book, print it.
+//	5. Otherwise, print that the book doesn't exist.
 //
-//  6. Handle the errors.
+//	6. Handle the errors.
 //
 // RESTRICTION:
-//   + The search should be case insensitive.
+//   - The search should be case insensitive.
 //
 // EXPECTED OUTPUT
-//   go run main.go
-//     Tell me a book title
 //
-//   go run main.go STAY
-//     Search Results:
-//     + Stay Golden
+//	go run main.go
+//	  Tell me a book title
 //
-//   go run main.go sTaY
-//     Search Results:
-//     + Stay Golden
+//	go run main.go STAY
+//	  Search Results:
+//	  + Stay Golden
 //
-//   go run main.go "Kafka's Revenge"
-//     Search Results:
-//     + Kafka's Revenge
-//     + Kafka's Revenge 2nd Edition
+//	go run main.go sTaY
+//	  Search Results:
+//	  + Stay Golden
 //
-//   go run main.go void
-//     Search Results:
-//     We don't have the book: "void"
+//	go run main.go "Kafka's Revenge"
+//	  Search Results:
+//	  + Kafka's Revenge
+//	  + Kafka's Revenge 2nd Edition
+//
+//	go run main.go void
+//	  Search Results:
+//	  We don't have the book: "void"
 //
 // HINTS:
-//   + To find out whether a string contains another string value, you can use the strings.Contains function.
-//   + To convert a string value to lowercase, you can use the strings.ToLower function.
-//   + Check out the strings package for more information.
+//   - To find out whether a string contains another string value, you can use the strings.Contains function.
+//   - To convert a string value to lowercase, you can use the strings.ToLower function.
+//   - Check out the strings package for more information.
+//
 // ---------------------------------------------------------
-
 func main() {
+	search_terms := os.Args[1:]
+	if len(search_terms) < 1 {
+		fmt.Println("Tell me a book title")
+		return
+	}
+	title := strings.ToLower(search_terms[0])
+	books := [...]string{
+		"Kafka's Revenge",
+		"Stay Golden",
+		"Everythingship",
+		"Kafka's Revenge 2nd Edition",
+	}
+	fmt.Println("Search Results:")
+	search_count := 0
+	for _, book := range books {
+		if strings.Contains(strings.ToLower(book), title) {
+			fmt.Printf("+ %s\n", book)
+			search_count += 1
+		}
+	}
+	if search_count == 0 {
+		fmt.Printf("We don't have the book: %q\n", title)
+	}
 }
