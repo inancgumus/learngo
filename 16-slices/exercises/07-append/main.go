f8cdf5655b2e7a094d2f1c3ab38253eec10a4f57
mindy <minzhou16@gmail.com>
2023-05-24 22:24:41 -0400

    added exercises for slices.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- 16-slices/exercises/07-append/main.go
+++ 16-slices/exercises/07-append/main.go
@@ -8,6 +8,11 @@
 
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Append
 //
@@ -22,12 +27,10 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// 1. uncomment the code below
-	// png, header := []byte{'P', 'N', 'G'}, []byte{}
+	png, header := []byte{'P', 'N', 'G'}, []byte{}
 
 	// 2. append elements to header to make it equal with the png slice
+	header = append(header, png...)
 
-	// 3. compare the slices using the bytes.Equal function
-
-	// 4. print whether they're equal or not
+	fmt.Printf("The two slices are equal: %t\n", bytes.Equal(header, png))
 }
--- 16-slices/exercises/09-append-3-fix/main.go
+++ 16-slices/exercises/09-append-3-fix/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Append #3 — Fix the problems
 //
@@ -24,12 +26,11 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// toppings := []int{"black olives", "green peppers"}
+	toppings := []string{"black olives", "green peppers"}
 
-	// var pizza [3]string
-	// append(pizza, ...toppings)
-	// pizza = append(toppings, "onions")
-	// toppings = append(pizza, extra cheese)
+	var pizza []string
+	pizza = append(pizza, toppings...)
+	pizza = append(pizza, "onions", "extra cheese")
 
-	// fmt.Printf("pizza       : %s\n", pizza)
+	fmt.Printf("pizza       : %s\n", pizza)
 }
